c7d846d6259d8f6d01431088d674338f2183cd49
Qiujie_Dong <qiujie_dong@mail.sdu.edu.cn>
2020-10-13 15:50:58 +0800

    change MeshCNN tran.sh

--- MeshCNN/scripts/human_seg/train.sh
+++ MeshCNN/scripts/human_seg/train.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 
 ## run the training
-"""
-dataroot：数据集所在的路径
-name:实验的名称，后续实验结果存储需要此名称，默认debug
-arch:网络的选择,默认mconvnet
-dataset_mode：数据集类型，默认为classification
-ncf：
-"""
+
 python train.py \
 --dataroot /data/MeshCNN/human_seg \
 --name human_seg \
