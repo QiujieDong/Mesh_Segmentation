bc82ad842d74b43f45afe7bb34372f4cbf1dfe28
Qiujie_Dong <qiujie_dong@mail.sdu.edu.cn>
2020-10-29 10:16:52 +0800

    add kaolin

---
+++ kaolin/kaolin/graphics/__init__.py
@@ -0,0 +1,8 @@
+from .Lighting import *
+from .nmr import *
+from .softras import *
+
+from .DifferentiableRenderer import DifferentiableRenderer
+from .NeuralMeshRenderer import NeuralMeshRenderer
+from .SoftRenderer import SoftRenderer
+from .DIBRenderer import DIBRenderer
