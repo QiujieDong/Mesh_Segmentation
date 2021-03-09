cd354c104a4a88116423f5beacea666d6e1bcaea
QiujieDong <dqj1249293688@gmail.com>
2021-03-09 10:46:17 +0800

    delete the MeshCNN code and work on building an awesome project

--- MeshCNN/scripts/coseg_seg/view.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-python util/mesh_viewer.py \
---files \
-checkpoints/coseg_vases/meshes/0288_0.obj \
-checkpoints/coseg_vases/meshes/0295_0.obj \
-checkpoints/coseg_vases/meshes/0299_0.obj \
