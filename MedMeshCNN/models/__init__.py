af949810d98620e70be2c08e654dcf1bff4271ab
QiujieDong <dqj1249293688@gmail.com>
2021-03-09 10:44:46 +0800

    delete the MesMeshCNN code and work on building an awesome project

--- MedMeshCNN/models/__init__.py
+++
@@ -1,4 +0,0 @@
-def create_model(opt):
-    from .mesh_classifier import ClassifierModel # todo - get rid of this ?
-    model = ClassifierModel(opt)
-    return model
--- MedMeshCNN/scripts/human_seg/view.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-python util/mesh_viewer.py \
---files \
-checkpoints/human_seg/meshes/shrec__14_0.obj
